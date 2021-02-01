bc9b597ce4db43a37443591f3bbddce5fd810cca
Valeriy Dichanskiy <dichansky@gmail.com>
2021-02-01 11:56:24 +0300

    now the value output has noraml appearence

--- proj/main.py
+++ proj/main.py
@@ -1,17 +1,20 @@
-from bs4 import BeautifulSoup as soup #парсит
-from urllib.request import urlopen as uReq #реквустит url
+from bs4 import BeautifulSoup as soup #parsing
+from urllib.request import urlopen as uReq #requesting url
+
 my_url = 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/'
-#Ищет указанный url и сканирует соответсвующий html страницы с указанным url
+#looking for url ans scanning suiteble html of the page with the set above url
 uClient = uReq(my_url)
 
-#Переврдит html в значение переменной
+#Turning html into text-variable
 page_html = uClient.read()
 
 uClient.close()
 
 #html parser
-page_soup = soup(page_html , "html.parser") #парсит html в html(не в xml,json и пр.)
-#print(page_soup.p)
-page_soup__directItem = page_soup.findAll("div",{"class":"game_description_snippet"})#ищет все элементы div на странице,с классом "game_description_snippet" 
+page_soup = soup(page_html , "html.parser") #parsing html into html(not xml,json and so on)
 
-print(page_soup__directItem)
+#print(page_soup.p)
+page_soup__directItem = page_soup.findAll("div",{"class":"game_purchase_price price"})#looking for every div with class on the page "game_description_snippet" 
+directItem__content = page_soup__directItem[0].text.strip() #Getting exact value of the container
+
+print("Actual product cost:",directItem__content)
